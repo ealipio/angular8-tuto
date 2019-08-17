73946277dd36f4e23d9811b42c6ec1d9b1cd876d
Eisson Alipio <eisson.alipio@gmail.com>
2019-08-17 16:43:29 -0500

    feat: adding core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { MaterialModule } from '../material/material.module';
+import { SharedModule } from '../shared/shared.module';
+import { ModalComponent } from './modal/modal.component';
+import { throwIfAlreadyLoaded } from './module-import-check';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SharedModule,
+    MaterialModule,
+    RouterModule // because we use <router-outlet> and routerLink
+  ],
+  declarations: [ModalComponent, ToolbarComponent],
+  exports: [ToolbarComponent],
+  entryComponents: [ModalComponent]
+})
+export class CoreModule {
+  constructor(
+    @Optional()
+    @SkipSelf()
+    parentModule: CoreModule
+  ) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+}
