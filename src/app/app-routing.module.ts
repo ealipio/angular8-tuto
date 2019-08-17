b454393c92b74a33b2835eb7801bc8c03bb1fb87
Eisson Alipio <eisson.alipio@gmail.com>
2019-08-17 16:24:01 -0500

    feat: adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'heroes' },
+  { path: 'heroes', loadChildren: 'app/heroes/heroes.module#HeroesModule' },
+  {
+    path: 'villains',
+    loadChildren: 'app/villains/villains.module#VillainsModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
