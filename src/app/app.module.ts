b1637bc04c20ba89d83e43155a365dda833fa1c9
Eisson Alipio <eisson.alipio@gmail.com>
2019-08-17 17:17:38 -0500

    feat: adding services and app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core';
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
-import { HeroListComponent } from './heroes/hero-list/hero-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroDetailComponent, HeroListComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    CoreModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
